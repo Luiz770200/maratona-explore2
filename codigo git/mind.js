71d6866c9966dcacd8864f9a9f2aaeebfdf75f9c
Luiz770200 <72815550+Luiz770200@users.noreply.github.com>
2022-07-07 02:07:21 -0300

    Add files via upload

---
+++ codigo git/mind.js
@@ -0,0 +1,11 @@
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+});
+
+const sex = document.getElementById('sex')
+
+sex.addEventListener('change', () => {
+    document.body.classList.toggle('pink')
+});
